9deda82ccb40cac88d42a15f1dbe4dc313fb4970
bearlen <zhoehberga@hotmail.com>
2022-03-22 22:44:17 +0800

    添加admin模块

---
+++ app/admin/controller/AdminBase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\BaseController;
+
+/**
+ * ADMIN模块基类
+ */
+class AdminBase extends BaseController
+{
+
+}
---
+++ app/admin/controller/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\BaseController;
+
+class Login extends BaseController
+{
+
+    /**
+     * 用户登录
+     */
+    public function sign()
+    {
+
+    }
+
+}
