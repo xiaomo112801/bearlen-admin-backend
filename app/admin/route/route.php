c432df07ec6fd525fb1b58c4213c5e26d3065811
bearlen <zhoehberga@hotmail.com>
2022-03-26 23:33:40 +0800

    配置apidoc

--- app/admin/route/route.php
+++ app/admin/route/route.php
@@ -4,5 +4,8 @@ use think\facade\Route;
 
 
 Route::group(function () {
+
     Route::get('user:uid', 'user/index');
 })->middleware(\app\middleware\JWT::class);
+
+Route::post('sign', 'login/sign');
