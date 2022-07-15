204968f84b5249a83db75ac18fb9165d2669469b
bearlen <zhoehberga@hotmail.com>
2022-07-15 18:08:31 +0800

    修改密码

--- app/admin/controller/User.php
+++ app/admin/controller/User.php
@@ -2,6 +2,8 @@
 
 namespace app\admin\controller;
 
+use think\exception\ValidateException;
+
 class User extends AdminBase
 {
 
@@ -13,9 +15,29 @@ class User extends AdminBase
 
     public function modifyPassword()
     {
+        try{
+            $old_password = input('post.oldPassword', '');
+            $new_password = input('post.newPassword', '');
+            $re_new_password = input('post.reNewPassword', '');
+
+            $rule = [
+                'oldPassword' => 'require',
+                'new_password' => 'require|length:8,16',
+                're_new_password' => 'require|confirm:password'
+            ];
+            $data = [
+                'oldPassword' => $old_password,
+                'new_password' => $new_password,
+                're_new_password' => $re_new_password
+            ];
+            $this->validate($data, $rule);
+
+
+        }catch (ValidateException $validateException){
+            return json(['message' => $validateException->getError()]);
+        }
 
     }
-
 
 
 }
